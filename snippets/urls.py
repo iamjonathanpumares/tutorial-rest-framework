9e861290bb78da8659e83e1d144290b7dd33076b
Jonathan Pumares <jepc1491@gmail.com>
2015-07-04 00:23:54 -0500

    Creamos un nuevo serializer para el modelo User que es para representar al usuario que creó el código snippet. Usamos vistas basadas en clases génericas ListAPIView y RetrieveAPIView para el modelo User y se agregaron los patrones de urls para las vistas hechas.

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
+	url(r'^users/$', views.UserList.as_view()),
+	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,26 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
